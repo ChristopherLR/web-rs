20f369d01276c66b7988b3fe6ddb82540c2deb79
Christopher Michael Sutton <christopher.sutton12@gmail.com>
2020-06-28 00:30:23 +1000

    adding more webpack stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./ui/index.ts",
   devtool: "inline-source-map",
+  devServer: { contentBase: "./dist" },
+  mode: "development",
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssests: false }),
+    new HtmlWebpackPlugin({ title: "development" }),
+  ],
   module: {
     rules: [
       {
