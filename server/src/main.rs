ebb17b86b8d18cbea295a80c4179d3899e9c4985
Christopher Michael Sutton <christopher.sutton12@gmail.com>
2020-06-29 01:00:21 +1000

    added file watcher

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,12 +2,14 @@
 #[macro_use]
 extern crate rocket;
 extern crate log;
+extern crate notify;
 extern crate ws;
 mod web;
 use log::{info, warn};
+use notify::{RecommendedWatcher, RecursiveMode, Watcher};
+use std::path::Path;
 use std::thread;
 use ws::{connect, listen, CloseCode, Handler, Message, Result, Sender};
-
 // Server WebSocket handler
 struct Server {
     out: Sender,
@@ -25,10 +27,35 @@ impl Handler for Server {
     }
 }
 
+fn watch() {
+    // Create a channel to receive the events.
+    let (tx, rx) = std::sync::mpsc::channel();
+
+    // Create a watcher object, delivering debounced events.
+    // The notification back-end is selected based on the platform.
+    let mut watcher: RecommendedWatcher =
+        Watcher::new_immediate(move |res| tx.send(res).unwrap()).unwrap();
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes.
+    watcher.watch("../dist", RecursiveMode::Recursive).unwrap();
+
+    loop {
+        match rx.recv() {
+            Ok(event) => info!("{:?}", event),
+            Err(e) => warn!("watch error: {:?}", e),
+        }
+    }
+}
+
 fn main() {
+    env_logger::init();
     let server = thread::spawn(move || loop {
         listen("127.0.0.1:3012", |out| Server { out }).unwrap()
     });
+
+    // Automatically select the best implementation for your platform.
+    thread::spawn(move || watch());
+
     rocket::ignite()
         .mount("/", routes![web::index, web::files])
         .launch();
